8998787b785eceb9242adb421d982d4ccb252080
Eigenaar <toonschaap3@gmail.com>
2018-05-18 10:53:29 +0200

    game

---
+++ game/SpaceProject/Assets/Scripts/Players/Playermovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playermovement : MonoBehaviour
+{
+	public float speed = 1.5f;
+
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
